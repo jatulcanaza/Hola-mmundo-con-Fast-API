4e30cf056728567962734218c87e27bb615c1b32
jatulcanaza <tulcanazajuan6@gmail.com>
2024-12-09 15:46:43 -0500

    main.py

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"mensaje": "¡Bienvenido a mi API!"}
+    return {"Hola Mundo": "¡Bienvenido a mi API!"}
 
-@app.get("/saludo/{nombre}")
-def saludar(nombre: str):
-    return {"mensaje": f"¡Hola, {nombre}!"}
